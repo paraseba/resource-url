357795dbba263cd586b86d7cf29959e3432e9dbd
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-05-28 01:25:09 -0300

    changed project version and description

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject resource-url "0.1.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject resource-url "0.1.0"
+  :description "Create URL generating functions"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
