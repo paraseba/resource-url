21eaa328b9b0cef1695038932cf6cd9de79a0c6d
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-05-28 13:03:35 -0300

    improved readme

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject resource-url "0.1.0"
-  :description "Create URL generating functions"
+  :description "Clojure library for URL generation"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
