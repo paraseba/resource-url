eeb8ee3ca26779b963eacd5cce13f98d75bf6139
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-05-29 07:38:02 -0300

    commented out autodoc dependency

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Clojure library for URL generation"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[autodoc "0.7.1"]])
+  );:dev-dependencies [[autodoc "0.7.1"]])
